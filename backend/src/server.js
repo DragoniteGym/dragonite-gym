efbf0f79e5af37a24ee00f13a90679abc65e14dd
Yoon Joo Woo <yjdream86@gmail.com>
2024-08-27 20:27:30 -0400

    added websocket logic for backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,41 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+// import http module
+const { createServer } = require('http');
+// import socket.io
+const { Server } = require('socket.io');
+
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen(port, () => {
+// create HTTP server using Express app
+const server = createServer(app);
+// attach socket.io to the server
+const io = new Server(server);
+
+// handle socket.io connections
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat meassage', (msg) => {
+    console.log('Message received: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+  //handle socket.io disconnect
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+
+
+
+
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
