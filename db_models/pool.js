02ec981f595d2bfd970fb14723abbdf49100fdd6
Elizabeth Mazorra Vidal <emazorra1@gmail.com>
2024-08-28 21:00:40 -0400

    added query object to export

--- db_models/pool.js
+++ db_models/pool.js
@@ -44,3 +44,10 @@ pool
     console.log(`SQL DB Connection Ended: ${client} removed from pool`);
    }
 );
+
+module.exports = {
+    query: (text, params, callback) => {
+        console.log('executed query', text);
+        return pool.query(text, params, callback);
+    }
+};
