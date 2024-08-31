9489a308e677bcf09d1730dd3e7b29e7eef5691a
Jennifer Slaughter <jslaughter@JennifesMacBook.lan>
2024-08-31 11:45:17 -0700

    comments for retrieving saved exercises

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,6 +1,6 @@
 /**
- * @module Profile
- * @description user's profile page (contains saved exercises)
+ * @module Exercises
+ * @description loads exercises from database
  */
 
 import React from 'react';
@@ -14,73 +14,87 @@ import CardActionArea from '@mui/material/CardActionArea';
 import dragonscales from '../assets/dragonscales.png';
 import exercisePhoto from '../assets/exercise.jpg';
 
-const bodyparts = [
+const workouts = [
     {
       url: exercisePhoto,
       title: 'Abs',
+      text: '',
+      link: '/search',
+      width: '30%',
     },
     {
       url: exercisePhoto,
       title: 'Arms',
+      text: '',
+      link: '/search',
+      width: '30%',
     },
     {
       url: exercisePhoto,
       title: 'Back',
+      text: '',
+      link: '/search',
+      width: '30%',
     },
     {
       url: exercisePhoto,
       title: 'Calves',
+      text: '',
+      link: '/search',
+      width: '30%',
     },
     {
       url: exercisePhoto,
       title: 'Cardio',
+      text: '',
+      link: '/search',
+      width: '30%',
     },
     {
       url: exercisePhoto,
       title: 'Chest',
+      text: '',
+      link: '/search',
+      width: '30%',
     },
     {
       url: exercisePhoto,
       title: 'Glutes',
-    },
-    {
-      url: exercisePhoto,
-      title: 'Legs',
-    },
-    {
-      url: exercisePhoto,
-      title: 'Shoulders',
+      text: '',
+      link: '/search',
+      width: '30%',
     },
   ];
 
-  const getExercises = (value) => {
-    alert(`Navigating to your saved ${value} exercises. Let's go bro!`);
-    // Use dispatch to update state with correct muscle group
-  }
+  // Create load exercises function
+    // Fetch exercises from DB using user_id and muscle group name (/exercises/?)
+        // Use useEffect to make API call when page renders?
+    // Load response from DB into array
+    // Populate cards with array info
 
-const Profile = () => {
+const Exercises = () => {
     return(
         <><div>
-        <p>This is the Profile Page</p>
-        <p><Link to='/home' id='home'>Home</Link></p>
+        <p>Here are your saved exercises!</p>
+        <p><Link to='/profile' id='profile'>Profile</Link></p>
         <p><Link to='/' id='landing'>Sign Out</Link></p>
         </div>
         <Box sx={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly',
         backgroundImage: `url(${dragonscales})`, minWidth: 300, width: '100%'}}>
-        {bodyparts.map((bodypart) => (
-            <Card sx={{ maxWidth: 345, minWidth: 250, margin: 2, width: '30%' }}>
-                <CardActionArea component={Link} to='/exercises' onClick={(e) => {getExercises(bodypart.title)}}>
+        {workouts.map((workout) => (
+            <Card sx={{ maxWidth: 345, minWidth: 250, margin: 2, width: workout.width }}>
+                <CardActionArea component={Link} to={workout.link}>
                     <CardMedia
                     component="img"
                     height="140"
-                    image={bodypart.url}
-                    alt={bodypart.title} />
+                    image={workout.url}
+                    alt={workout.title} />
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="div">
-                            {bodypart.title}
+                            {workout.title}
                         </Typography>
                         <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                            Find {bodypart.title} exercises here!
+                            {workout.text}
                         </Typography>
                     </CardContent>
                 </CardActionArea>
@@ -90,4 +104,4 @@ const Profile = () => {
     )
 };
 
-export default Profile;
+export default Exercises;
