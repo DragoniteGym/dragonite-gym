4ea168228f28f2dfef63d9f735394f8a65bfdd5e
Jennifer Slaughter <jslaughter@JennifesMacBook.lan>
2024-09-04 20:30:15 -0700

    functional api call to get saved workouts

--- server/controllers/savedWorkoutsController.js
+++ server/controllers/savedWorkoutsController.js
@@ -7,7 +7,26 @@ const getWorkouts = async (req, res) => {
         //const result = await pool.query('SELECT * FROM users', []);
         console.log('Category:', bodypart);
         console.log('User:', user);
-        res.status(200).json({ message: 'Success in saved workouts' });
+        res.status(200).json([
+            {
+                title: 'Exercise #Yay',
+                text: '',
+                link: '/search',
+                width: '30%',
+            },
+            {
+                title: 'Exercise #It Worked!',
+                text: '',
+                link: '/search',
+                width: '30%',
+            },
+            {
+                title: `Exercise # ${bodypart}`,
+                text: '',
+                link: '/search',
+                width: '30%',
+            },
+        ]);
     } catch (err) {
         console.log('Error in getWorkouts:', err);
         res.status(500).json({ message: 'Error getting saved workouts' });
