f8fad22de0da304ad03925fa79d3687e8588961d
Yoon Joo Woo <yjdream86@gmail.com>
2024-09-07 13:25:41 -0400

    applied background and navbar to the search page

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,13 +5,26 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import dragonscales from '../assets/dragonscales.png';
 import Navbar from './NavBar.jsx';
 
 const Search = () => {
     return(
-        <div>
-            <Navbar />
-        </div>
+        <>
+            <Box sx={{
+                backgroundImage: `url(${dragonscales})`,
+                backgroundPosition: 'center',
+                backgroundRepeat: 'repeat',
+                minHeight: '100vh',
+                height: 'auto',
+                display: 'flex',
+                flexDirection: 'column',
+                }}>
+                <Navbar />
+            
+            </Box>
+        </>
     )
 };
 
